3ee0b57c611f4d15d433fc873e2e3952c9540d02
kimi <kimi0230@gmail.com>
2018-12-11 11:24:23 +0800

    輪播

--- pages/home/index.js
+++ pages/home/index.js
@@ -8,28 +8,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swipers:[],
-    logos:[],
-    quicks:[],
-    pageRow:[]
+    swipers: [],
+    logos: [],
+    quicks: [],
+    pageRow: [],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 500
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     const self = this;
 
     wx.showLoading({
-      title:"Loading...",
+      title: "Loading...",
     })
 
     wx.request({
-      url:interfaces.homepage,
-      header:{
-        "content-type":"application/json"
+      url: interfaces.homepage,
+      header: {
+        "content-type": "application/json"
       },
-      success(res){
+      success(res) {
         console.log(res.data);
         console.log(self);
         self.setData({
@@ -47,49 +52,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
