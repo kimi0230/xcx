cb1d6b62d3b9b2cac490f588232686c419156ca5
kimi <kimi0230@gmail.com>
2018-12-16 14:24:01 +0800

    15: 展示商品列表

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,3 +1,5 @@
+const interfaces = require("../../utils/urlconfig.js");
+
 // pages/list/index.js
 Page({
 
@@ -5,65 +7,92 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    prolist:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // console.log(options);
     wx.setNavigationBarTitle({
-      title:options.title
+      title: options.title
     });
-  },
 
+    const self = this;
+
+    wx.showLoading({
+      title: "Loading...",
+    })
+
+    wx.request({
+      url: interfaces.productionsList,
+      header: {
+        "content-type": "application/json"
+      },
+      success(res) {
+        // console.log(res.data);
+        // console.log(self);
+        self.setData({
+          prolist: res.data
+        })
+      }
+    })
+
+    wx.hideLoading();
+    
+  },
+  switchProlistDetail:function(e){
+    console.log(e);
+    let index = e.currentTarget.dataset.index;
+    
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
